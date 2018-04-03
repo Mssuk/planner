d4c1981ca1a518b931e3d53eb9d489a0bfd48841
Mssuk <djqhr490@naver.com>
2018-04-03 14:36:05 +0900

    node on android2

---
+++ node-on-android/example/app/index.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+var path = require('path');
+var android = require('node-on-android');
+
+app.use(express.static(path.join(__dirname, 'js')));
+
+android.loadUrl('http://localhost:3000');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
+
